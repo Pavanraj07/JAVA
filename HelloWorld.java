b60df8515396acea1254c26381813745e3766192
Pavanraj07 <pavanrajcsd152@gmail.com>
2025-01-04 20:11:47 +0530

    first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 // Simple Java Program
 public class HelloWorld {
     public static void main(String[] args) {
-        // Print a message to the console
         System.out.println("Hello, World!");
     }
 }
